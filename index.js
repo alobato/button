8adcb74b3e3d0bfe162fdf54f1768127a997d3bc
Arley Lobato <arleylobato@gmail.com>
2019-08-08 00:32:58 +0100

    Add return

--- index.js
+++ index.js
@@ -13,20 +13,22 @@ const ButtonRaw = styled.button`
   outline: none;
 `
 
-const Button = ({ children, loading, primary, small, ...rest}) => (
-  <ButtonRaw {...rest}>
-    <i>
-      <svg height={16} viewBox='0 0 100 100' preserveAspectRatio='xMidYMid'>
-        <circle cx='50' cy='50' fill='none' stroke='currentColor' strokeWidth='10' r='35' strokeDasharray='164.93361431346415 56.97787143782138' transform='rotate(305.844 50 50)'>
-          <animateTransform attributeName='transform' type='rotate' calcMode='linear' values='0 50 50;360 50 50' keyTimes='0;1' dur='1s' begin='0s' repeatCount='indefinite' />
-        </circle>
-      </svg>
-    </i>
-    <span style={{display: 'inline-block', pointerEvents: 'none'}}>
-      {children}
-    </span>
-  </ButtonRaw>
-)
+const Button = ({ children, loading, primary, small, ...rest}) => {
+  return (
+    <ButtonRaw {...rest}>
+      <i>
+        <svg height={16} viewBox='0 0 100 100' preserveAspectRatio='xMidYMid'>
+          <circle cx='50' cy='50' fill='none' stroke='currentColor' strokeWidth='10' r='35' strokeDasharray='164.93361431346415 56.97787143782138' transform='rotate(305.844 50 50)'>
+            <animateTransform attributeName='transform' type='rotate' calcMode='linear' values='0 50 50;360 50 50' keyTimes='0;1' dur='1s' begin='0s' repeatCount='indefinite' />
+          </circle>
+        </svg>
+      </i>
+      <span style={{display: 'inline-block', pointerEvents: 'none'}}>
+        {children}
+      </span>
+    </ButtonRaw>
+  )
+}
 
 export default styled(Button)`
   font-family: 'Poppins';
