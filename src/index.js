a57e1951eaa539447f49f6b6086e0c384fffc6ff
Arley Lobato <arleylobato@gmail.com>
2019-08-20 22:44:20 +0100

    Remove theme dependencies

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ const Button = ({ children, loading, primary, small, ...rest}) => {
 }
 
 export default styled(Button)`
-  font-family: 'Poppins';
+  font-family: ${props => (props.theme && props.theme.secondaryFontFamily) ? props.theme.secondaryFontFamily : 'inherit'};
   border-radius: 22px;
   font-size: 16px;
   font-weight: 700;
@@ -41,7 +41,7 @@ export default styled(Button)`
   box-shadow: 0 2px 0 rgba(0, 0, 0, 0.045);
   color: ${props => props.theme.colors.white};
   text-shadow: 0 -1px 0 rgba(0, 0, 0, 0.12);
-  background-color: ${props => props.theme.colors.primary};
+  background-color: ${props => (props.theme && props.theme.color && props.theme.colors.primary) ? props.theme.colors.primary : 'hsla(207, 73%, 57%, 1)'};
   ${props => props.small && css`
     font-size: 15px;
     font-weight: 600;
@@ -49,13 +49,13 @@ export default styled(Button)`
     padding: 8px 24px;
   `}
   ${props => props.loading && css`
-    background-color: ${props => props.theme.colors.primary300};
+    background-color: ${props => (props.theme && props.theme.color && props.theme.colors.primary300) ? props.theme.colors.primary300 : 'hsla(203, 82%, 76%, 1)'};
     pointer-events: none;
   `}
   &:focus {
     outline: 0;
-    box-shadow: 0 0 0 3px ${props => props.theme.colors.primary200};
-    border: 1px solid ${props => props.theme.colors.primary400};
+    box-shadow: 0 0 0 3px ${props => (props.theme && props.theme.color && props.theme.colors.primary200) ? props.theme.colors.primary200 : 'hsla(202, 81%, 86%, 1)'};
+    border: 1px solid ${props => (props.theme && props.theme.color && props.theme.colors.primary400) ? props.theme.colors.primary400 : 'hsla(205, 79%, 66%, 1)'};
   }
   & > i {
     display: inline-block;
